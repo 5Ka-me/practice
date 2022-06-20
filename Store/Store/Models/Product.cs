a1e2d394a9eefa094e2ef79959ac8c2c6921b21a
5Ka <73385569+5Ka-me@users.noreply.github.com>
2022-06-20 18:01:00 +0300

    Feature/add bussines logic (#2)
    * Empty project
    
    * Add EF
    
    * Add CRUD
    
    * Change according to PR
    
    * Add validation
    
    * Add simpe business logic
    
    * Added simple business logic

--- Store/Store/Models/Product.cs
+++ Store/Store/Models/Product.cs
@@ -7,6 +7,8 @@ namespace Store.Models
         [Key]
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public string Description { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public bool IsOnSale { get; set; }  = false;
     }
 }
---
+++ Store/Store/Models/ProductInStock.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models
+{
+    public class ProductInStock
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+    }
+}
