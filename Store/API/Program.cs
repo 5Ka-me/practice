264b5bed312bfe6ecd68ae167bdeae028563bedd
5Ka <73385569+5Ka-me@users.noreply.github.com>
2022-06-27 12:26:39 +0300

    Added automapper (#6)

--- Store/API/Program.cs
+++ Store/API/Program.cs
@@ -1,9 +1,10 @@
 using API.Models;
+using API.Profiles;
 using BLL.Interfaces;
 using BLL.Services;
 using DAL;
-using DAL.Repositories;
 using DAL.Interfaces;
+using DAL.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,8 +15,9 @@ builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(conn
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-// Add services to the container.
+builder.Services.AddAutoMapper(typeof(ProductProfile));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
