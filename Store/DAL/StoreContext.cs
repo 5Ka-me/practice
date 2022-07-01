ffd24f2b1223783def8f49c6e7327f3ec0c2f748
5Ka <dima.bogdanovich.02@mail.ru>
2022-07-01 17:24:30 +0300

    Renamed async methods and added var's

--- Store/DAL/StoreContext.cs
+++ Store/DAL/StoreContext.cs
@@ -19,19 +19,5 @@ namespace DAL
                 optionsBuilder.UseSqlServer("DefaultConnection");
             }
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Category>().HasData(
-                new Category { Id = 1, Name = "Motherboard" },
-                new Category { Id = 2, Name = "Processor" }
-            );
-
-            modelBuilder.Entity<Product>().HasData(
-                new Product { Id = 1, CategoryId = 1, Name = "Motherboard1", Description = "ProductDescription1", Price = 15, IsOnSale = false },
-                new Product { Id = 2, CategoryId = 2, Name = "Processor1", Description = "ProductDescription2", Price = 25, IsOnSale = false },
-                new Product { Id = 3, CategoryId = 2, Name = "Processor2", Description = "ProductDescription3", Price = 35, IsOnSale = false }
-            );
-        }
     }
 }
